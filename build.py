4dcea75b2f4c79dea13f0497e46c56f60439d990
Shiv2205 <shivamveerabudren@gmail.com>
2025-03-28 20:44:52 -0400

    Updated build configurations

--- build.py
+++ build.py
@@ -21,9 +21,18 @@ def main():
 
     elif type == "build":
         if build == "full":
-            subprocess.run(["cmake", "-DCMAKE_TOOLCHAIN_FILE=toolchain.cmake", "-S", ".", "-B", "./build"])
+          subprocess.run(["cmake",
+          "-DCMAKE_BUILD_TYPE=Release",
+          "-DCMAKE_TOOLCHAIN_FILE=toolchain.cmake", "-S", ".", "-B", "./build"])
 
-        subprocess.run(["cmake", "--build", "./build"])
+        elif build == "debug":
+          build_type = "Debug"
+          subprocess.run(["cmake", 
+                        "-DCMAKE_BUILD_TYPE=" + build_type,
+                        "-S", ".", 
+                        "-B", "./build"])
+
+        subprocess.run(["cmake", "--build", "./build"])      
 
     elif type == "run":
         subprocess.run(["./build/bin/sfml_game"])
